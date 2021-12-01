0b768e58d512a5e2e6f09240357802112950d737
Amine <resa@booking-tunisie.com>
2021-12-01 09:38:56 +0100

    BF service empty config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  backendUri : "http://localhost:8080/api/",
-  authorization : "Basic YWRtaW46MTIzNA=="
+  backendUri : "http://localhost:8080/api/"
 };
