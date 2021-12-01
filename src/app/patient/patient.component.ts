7e5c4234fc4e20b7e2b191f2bbb8b89c68fd9b16
Amine <resa@booking-tunisie.com>
2021-12-01 12:22:19 +0100

    commit intermédiaire

--- src/app/patient/patient.component.ts
+++ src/app/patient/patient.component.ts
@@ -7,17 +7,20 @@ import { Patient } from '../classes/patient';
 import { Ville } from '../classes/ville';
 import { ConfigService } from '../services/config.service';
 import { PatientService } from '../services/patient.service';
+import { RdvService } from '../services/rdv.service';
 import { VilleService } from '../services/ville.service';
 
 @Component({
   selector: 'app-patient',
   templateUrl: './patient.component.html',
   styleUrls: ['./patient.component.css'],
-  providers:[ConfigService]
+  providers: [ConfigService]
 })
 export class PatientComponent implements OnInit {
 
   patient: Patient = new Patient()
+  patientRdv : Array<any> = []
+
   patients: Array<Patient> = []
   villes: Array<Ville> = []
   search: string = ""
@@ -26,12 +29,21 @@ export class PatientComponent implements OnInit {
 
   @ViewChild('closebutton') closebuttonelement: any;
 
-  constructor(private vs: VilleService, private ps: PatientService 
-    , public config : ConfigService) {
+  constructor(private vs: VilleService, private ps: PatientService , private rdvs: RdvService
+    , public config: ConfigService) {
+  }
+
+
+  getPatientRdv(): void {
+    this.patientRdv = [];
+
+
+
   }
 
   ngOnInit(): void {
     this.reloadPatients()
+    this.getPatientRdv()
 
     this.vs.getAll().subscribe({
       next: (data) => { this.villes = data },
@@ -39,10 +51,32 @@ export class PatientComponent implements OnInit {
     });
   }
 
+
+  // PatientRDV*
+  /*[
+    1 = [ rdv1, rdv2 ]
+    10 = [rdv10]
+  ]*/
+
   reloadPatients() {
-    console.log( this.config.httpOptions.headers )
+    console.log(this.config.httpOptions.headers)
     this.ps.getAll(this.search).subscribe(
-      data => { this.patients = data }
+      data => { 
+        this.patients = data 
+
+        // je boucle sur les patients pour faire une req http et récupérer les rdv de chaq patient
+        data.forEach( patient => {
+          this.rdvs.getAll(undefined , patient.id ).subscribe(
+            drdv => { 
+              if( patient.id != undefined )
+                this.patientRdv[ patient.id  ] = drdv 
+              }
+          )
+          
+        }  )
+
+        console.log( this.patientRdv )
+      }
       //, err => console.log( "Une erreur est survenue" )
     );
   }
